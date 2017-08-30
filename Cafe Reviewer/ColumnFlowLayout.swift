8106d7b9e3d56de2f9152dc287ea19fe81313be6
Lauren Grimes <lauren.grimes@gmail.com>
2017-08-30 18:25:37 +1000

    First page completed

---
+++ Cafe Reviewer/ColumnFlowLayout.swift
@@ -0,0 +1,46 @@
+//
+//  ColumnFlowLayout.swift
+//  Cafe Reviewer
+//
+//  Created by Lauren Grimes on 30/8/17.
+//  Copyright © 2017 Code like a girl. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColumnFlowLayout: UICollectionViewFlowLayout {
+    
+    let cellsPerRow: Int
+    override var itemSize: CGSize {
+        get {
+            guard let collectionView = collectionView else { return super.itemSize }
+            let marginsAndInsets = sectionInset.left + sectionInset.right + minimumInteritemSpacing * CGFloat(cellsPerRow - 1)
+            let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(cellsPerRow)).rounded(.down)
+            return CGSize(width: itemWidth, height: itemWidth)
+        }
+        set {
+            super.itemSize = newValue
+        }
+    }
+    
+    init(cellsPerRow: Int, minimumInteritemSpacing: CGFloat = 0, minimumLineSpacing: CGFloat = 0, sectionInset: UIEdgeInsets = .zero) {
+        self.cellsPerRow = cellsPerRow
+        super.init()
+        
+        self.minimumInteritemSpacing = minimumInteritemSpacing
+        self.minimumLineSpacing = minimumLineSpacing
+        self.sectionInset = sectionInset
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func invalidationContext(forBoundsChange newBounds: CGRect) -> UICollectionViewLayoutInvalidationContext {
+        let context = super.invalidationContext(forBoundsChange: newBounds) as! UICollectionViewFlowLayoutInvalidationContext
+        context.invalidateFlowLayoutDelegateMetrics = newBounds != collectionView?.bounds
+        return context
+    }
+    
+}
--- Cafe Reviewer/Restaurant.swift
+++ Cafe Reviewer/Restaurant.swift
@@ -9,33 +9,14 @@
 import Foundation
 
 class Restaurant {
-    var name: String
-    var urlString: String
-    var address: String
-    var latitude: String
-    var longitude: String
-    var cuisine: String
-    var rating: String
-    var thumbnailUrlString: String
-    var menuUrlString: String
-    
-    init(name: String = "",
-         urlString: String = "",
-         address: String = "",
-         latitude: String = "",
-         longitude: String = "",
-         cuisine: String = "",
-         rating: String = "",
-         thumbnailUrlString: String = "",
-         menuUrlString: String = "") {
-        self.name = name
-        self.urlString = urlString
-        self.address = address
-        self.latitude = latitude
-        self.longitude = longitude
-        self.cuisine = cuisine
-        self.rating = rating
-        self.thumbnailUrlString = thumbnailUrlString
-        self.menuUrlString = menuUrlString
-    }
+    var name: String = ""
+    var urlString: String = ""
+    var address: String = ""
+    var latitude: String = ""
+    var longitude: String = ""
+    var cuisine: String = ""
+    var rating: String = ""
+    var thumbnailUrlString: String = ""
+    var menuUrlString: String = ""
+    var featuredImageUrlString: String = ""
 }
